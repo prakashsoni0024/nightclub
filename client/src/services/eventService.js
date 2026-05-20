83479e7f3eda9d65bf43a0470a4d86b46907769a
prakash soni <prakashsoni0024@gmail.com>
2026-05-20 17:47:57 +0530

    add slash on event

--- client/src/services/eventService.js
+++ client/src/services/eventService.js
@@ -2,18 +2,18 @@ import API from "./api";
 
 // GET events
 export const getEvents = async () => {
-  const res = await API.get("events");
+  const res = await API.get("/events");
   return res.data;
 };
 
 // CREATE event
 export const createEvent = async (data) => {
-  const res = await API.post("events", data);
+  const res = await API.post("/events", data);
   return res.data;
 };
 
 // DELETE event
 export const deleteEvent = async (id) => {
-  const res = await API.delete(`events/${id}`);
+  const res = await API.delete(`/events/${id}`);
   return res.data;
 };
