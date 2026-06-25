d11fcab270be47866d1133463162f6eb6f87cdea
prakash soni <prakashsoni0024@gmail.com>
2026-06-25 20:34:43 +0530

    bug fix

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   requireTLS: true,
   auth: {
     user: process.env.BREVO_USER,
